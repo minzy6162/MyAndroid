4058ab4a855fb005f63525b561efb4de987de974
Minjung Choi <s2019s37@e-mirim.hs.kr>
2020-09-24 20:51:22 +0900

    p104 버튼 실행 예제

---
+++ app/src/main/java/kr/hs/emirim/minjung/myandlab/MainActivity.kt
@@ -0,0 +1,35 @@
+package kr.hs.emirim.minjung.myandlab
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.security.identity.AccessControlProfile
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mProfile : ImageView
+    lateinit var mButton1 : Button
+    lateinit var mButton2 : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mProfile = findViewById(R.id.profile)
+        mButton1 = findViewById(R.id.button1)
+        mButton2 = findViewById(R.id.button2)
+
+        mButton1.setOnClickListener {
+            val intent = Intent(this, FourbuttonActivity::class.java)
+            //Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent)
+        }
+
+        mButton2.setOnClickListener {
+            val intent = Intent(this, Fourbutton2Activity::class.java)
+            startActivity(intent)
+        }
+    }
+}
